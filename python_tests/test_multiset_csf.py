60bc3cb1f9ed30ef07e23cfde716f16cd0db9258
David Torres <detorresramos1@gmail.com>
2023-12-09 21:24:25 -0600

    different length values test

--- python_tests/test_multiset_csf.py
+++ python_tests/test_multiset_csf.py
@@ -24,3 +24,14 @@ def test_multiset_csf():
 
     for key, value in zip(keys, values):
         assert csf.query(key) == value
+
+
+def test_multiset_csf_different_number_of_values():
+    keys = ["1", "2", "3"]
+    values = [[1, 2], [2, 3], [3]]
+
+    with pytest.raises(
+        ValueError,
+        match="Error transforming values to column-wise. Make sure all values are the same length.",
+    ):
+        caramel.MultisetCSF(keys, values)
