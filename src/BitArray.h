bcc5f3a80c080c3107c5d580c4ddd28861ca8081
david <david@thirdai.com>
2024-05-14 22:22:36 +0000

    refactor bitarray

--- src/BitArray.h
+++ src/BitArray.h
@@ -9,20 +9,13 @@
 
 namespace caramel {
 
-/* make CHAR_BIT 8 if it's not defined in limits.h */
-#ifndef CHAR_BIT
-// #warning CHAR_BIT not defined.  Assuming 8 bits.
-#define CHAR_BIT 8
-#endif
+#define BLOCK_SIZE 64
 
-/* array index for character containing bit */
-#define BIT_CHAR(bit) ((bit) / CHAR_BIT)
+/* array index for block containing bit */
+#define BIT_BLOCK(bit) ((bit) / BLOCK_SIZE)
 
 /* position of bit within character */
-#define BIT_IN_CHAR(bit) (1 << (CHAR_BIT - 1 - ((bit) % CHAR_BIT)))
-
-/* number of characters required to contain number of bits */
-#define BITS_TO_CHARS(bits) ((((bits)-1) / CHAR_BIT) + 1)
+#define BIT_IN_BLOCK(bit) (1ULL << (BLOCK_SIZE - 1ULL - ((bit) % BLOCK_SIZE)))
 
 class BitArray;
 using BitArrayPtr = std::shared_ptr<BitArray>;
@@ -37,13 +30,13 @@ public:
     return std::make_shared<BitArray>(num_bits);
   }
 
-  static std::shared_ptr<BitArray> fromNumber(uint32_t int_value,
+  static std::shared_ptr<BitArray> fromNumber(uint64_t int_value,
                                               uint32_t length);
 
   uint32_t numBits() const { return _num_bits; }
 
   bool operator[](const uint32_t index) const {
-    return ((_backing_array[BIT_CHAR(index)] & BIT_IN_CHAR(index)) != 0);
+    return ((_backing_array[BIT_BLOCK(index)] & BIT_IN_BLOCK(index)) != 0);
   }
 
   void clearBit(uint32_t index);
@@ -54,6 +47,8 @@ public:
 
   BitArray &operator&=(const BitArray &other);
 
+  BitArray operator^(const BitArray &other) const;
+
   BitArray operator&(const BitArray &other) const;
 
   BitArray &operator=(const BitArray &other);
@@ -61,11 +56,13 @@ public:
   bool operator==(const BitArray &other) const;
 
   // index of first nonzero bit, std::nullopt otherwise
-  // TODO unit test this one
   std::optional<uint32_t> find() const;
 
   // true if any bits are 1 and false otherwise
-  bool any() const { return find().has_value(); }
+  bool any() const {
+    return !std::all_of(_backing_array.begin(), _backing_array.end(),
+                       [](uint64_t block) { return block == 0; });
+  }
 
   void setAll();
 
@@ -83,6 +80,8 @@ public:
 
   std::string str() const;
 
+  std::vector<uint64_t> backingArray() const { return _backing_array; }
+
 private:
   // Private constructor for cereal
   BitArray() {}
@@ -91,26 +90,9 @@ private:
   template <class Archive> void serialize(Archive &archive);
 
   uint32_t _num_bits;
-  uint32_t _num_bytes;
 
-  // This is a vector because cereal doesn't allow serialization of pointers
-  // (ex. char*).
-  std::vector<unsigned char> _backing_array;
-
-  static const std::vector<int> _set_bits_in_char;
+  // Use a vector since cereal doesn't allow serialization of pointers
+  std::vector<uint64_t> _backing_array;
 };
-
-inline const std::vector<int> BitArray::_set_bits_in_char = {
-    0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4,
-    2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4,
-    2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6,
-    4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5,
-    3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6,
-    4, 5, 5, 6, 5, 6, 6, 7, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-    4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8};
 
 } // namespace caramel
